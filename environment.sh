22bd00138e95a02d775bddfb2897a08a0745da84
Giles Knap <giles.knap@diamond.ac.uk>
2024-08-24 08:11:02 +0100

    adopt copier template

--- environment.sh
+++ environment.sh
@@ -10,8 +10,13 @@
 
 # This script must be sourced
 if [ "$0" = "$BASH_SOURCE" ]; then
-    echo "ERROR: Please source this script (source environment.sh)"
+    echo "ERROR: Please source this script (source ./environment.sh)"
     exit 1
+fi
+
+# if there is a docker-compose module then load it
+if [[ $(module avail docker-compose 2>/dev/null) != "" ]] ; then
+    module load docker-compose
 fi
 
 function check_docker {
@@ -22,9 +27,11 @@ function check_docker {
     fi
 }
 
+# set user id for the phoebus container for easy X11 forwarding.
 if check_docker; then
     USER_ID=$(id -u); USER_GID=$(id -g)
 else
+    alias docker=podman
     USER_ID=0; USER_GID=0
 fi
 
@@ -35,9 +42,9 @@ xhost +SI:localuser:$(id -un)
 
 # set user id for the phoebus container for easy X11 forwarding.
 export UIDGID=$USER_ID:$USER_GID
-# choose develop profile for docker compose
-export COMPOSE_PROFILES=develop
-# for develop profile our ca-gateway publishes PVS on the loopback interface
+# choose test profile for docker compose
+export COMPOSE_PROFILES=test
+# for test profile our ca-gateway publishes PVS on the loopback interface
 export EPICS_CA_ADDR_LIST=127.0.0.1
 # make a short alias for docker-compose for convenience
 alias ec='docker compose'
